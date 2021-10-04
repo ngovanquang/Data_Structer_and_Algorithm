2b8ed91d9a12a4691a05a769c4e8780d73955368
ngovanquang <banbanhbeo@outlook.com>
2021-10-04 17:44:14 +0700

    improving the solution

--- Array/Valid_Mountain_Array.cpp
+++ Array/Valid_Mountain_Array.cpp
@@ -7,30 +7,18 @@ public:
     bool validMountainArray(vector<int>& arr) {
        int length = arr.size();
        if (length <= 2) return false;
-       bool isTang = false;
-       bool isGiam = false;
-       bool isDinh = false;
        int pivot = 0;
        for (int i = 0; i < length-2; i++) {
            if (arr[i] < arr[i+1] && arr[i+1] < arr[i+2]) {
-               isTang = true;
-               isGiam = false;
-               isDinh = false;
+               pivot = i + 2;
            } else if (arr[i] < arr[i+1] && arr[i+1] > arr[i+2]) {
-               isTang = false;
-               isGiam = false;
-               isDinh = true;
+               pivot = i + 1;
            } else if (arr[i] > arr[i+1] && arr[i+1] > arr[i+2]) {
-               isTang = false;
-               isGiam = true;
-               isDinh = false;
+               if (pivot == 0) return false;
            } else return false;
-
-           if (isTang) pivot = i+2;
-           else if (isDinh) pivot = i+1;
            
-           // Truong hop mang chi tang hoac giam
-           if (pivot == 0 || pivot == (length - 1))
+           // Truong hop mang chi tang
+           if ( pivot == (length - 1))
                return false;
        }
         return true;
@@ -39,7 +27,7 @@ public:
 
 int main(int argc, char ** argv)
 {
-    vector<int> arr = {0,3,2,1};
+    vector<int> arr = {0,1,2,1,2};
     Solution solution = Solution();
     cout << solution.validMountainArray(arr) << endl;
     return 0;
